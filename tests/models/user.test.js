3b7fb64b0fa0136947cfc891feed6a650f3e3dac
walid <walid.eddous@gmail>
2020-02-20 00:35:52 +0100

    unit test

---
+++ tests/models/user.test.js
@@ -0,0 +1,14 @@
+const { User } = require("../../models/user");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const mongoose = require("mongoose");
+
+describe("generateAuthToken", () => {
+  it("should return a valid JWT", async () => {
+    const payload = { id: new mongoose.Types.ObjectId().toHexString() };
+    const user = new User(payload);
+    const resultat = user.generateAuthToken();
+    const decoded = await jwt.verify(resultat, config.get("jsonSecretKey"));
+    expect(decoded).toMatchObject(payload);
+  });
+});
